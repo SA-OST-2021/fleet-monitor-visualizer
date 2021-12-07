d5df33b877e6cac00f6a4bb81a469cc8ce4e8ea2
Florian Baumgartner <florian.b-h@bluewin.ch>
2021-12-07 17:21:49 +0100

    Added Plots as svg and png

--- CAN-Dump Converter/CAN-Dump Converter.py
+++ CAN-Dump Converter/CAN-Dump Converter.py
@@ -67,7 +67,6 @@ with open(fileName) as file:
             s[f"{n}"] = int(l[4 + n], 16)
         data.append(s)
 
-
 df = pd.DataFrame(data, columns=["time", "priority", "pgn", "source", "data",
                                  "0", "1", "2", "3", "4", "5", "6", "7"])
 df["date"] = pd.to_datetime(df["time"], unit="s")
@@ -77,6 +76,10 @@ for pgn in pgnTypes:
         print(f"{pgn:04X}: " + pgnNameTable[f"{pgn:04X}"])
     else:
         print(f"{pgn:04X}")
+
+
+df = df[df["date"] > '2021-11-17 16:00:00']  # Only for generating visualization!
+
 
 
 tachograph = df[df["pgn"] == 0xFE6C]
@@ -111,37 +114,162 @@ suspension["suspension"] = (suspension["0"] +
                             suspension["6"] * 256**6  +
                             suspension["7"] * 256**7) / 10.0
 
+temperature = df[df["pgn"] == 0xFEEE]
+temperature["temperature"] = temperature["0"] - 40.0
 
-fig = make_subplots(rows=5, cols=1, shared_xaxes=True)
+diselExhaustFluid = df[df["pgn"] == 0xFE56]
+diselExhaustFluid["diselExhaustFluid"] = diselExhaustFluid["0"] * 0.4
+
+ambientAir = df[df["pgn"] == 0xFEF5]
+ambientAir["ambientAir"] = (ambientAir["3"] + ambientAir["4"] * 256) * 0.03125 - 273.0
+
+alternator = df[df["pgn"] == 0xFED5]
+alternator["alternatorSpeed"] = (alternator["0"] + alternator["1"] * 256) / 8.0
+alternatorStatus = {0x00: "not charging", 0x01: "charging", 0x02: "error", 0x03: "not available"}
+alternator["alternator1"] = pd.Series((alternator["2"] & 0x03) // 1).map(alternatorStatus)
+alternator["alternator2"] = pd.Series((alternator["2"] & 0x0C) // 4).map(alternatorStatus)
+alternator["alternator3"] = pd.Series((alternator["2"] & 0x30) // 16).map(alternatorStatus)
+alternator["alternator4"] = pd.Series((alternator["2"] & 0xC0) // 64).map(alternatorStatus)
+
+vehicleDistance = df[df["pgn"] == 0xFEC1]
+vehicleDistance["vehicleDistance"] = (vehicleDistance["0"] + 
+                                      vehicleDistance["1"] * 256 + 
+                                      vehicleDistance["2"] * 256**2  +
+                                      vehicleDistance["3"] * 256**3) * 5.0
+vehicleDistance["vehicleDistance"] -= vehicleDistance["vehicleDistance"].iloc[0]
+
+airPressure = df[df["pgn"] == 0xFEAE]
+airPressure["airPressure"] = (airPressure["2"] + airPressure["3"] * 256) * 8.0
+# airPressure["airPressure"] -= min(airPressure["airPressure"])
+
+cruiseControl = df[df["pgn"] == 0xFEF1]
+# cruiseControl["parkBreak"] = pd.Series((cruiseControl["0"] & 0x04) // 4)
+cruiseControl["wheelSpeed"] = cruiseControl["1"] + cruiseControl["2"] / 256.0
+cruiseControl["clutchSwitch"] = pd.Series((cruiseControl["3"] & 0x40) // 64)
+cruiseControl["breakSwitch"] = pd.Series((cruiseControl["3"] & 0x10) // 16)
+cruiseControl["cruiseControl"] = pd.Series(cruiseControl["3"] & 0x01)
+cruiseControlStatus = {0x00: "off", 0x01: "hold", 0x02: "accelerate", 0x03: "decelerate", 0x04: "resume", 0x05: "set", 0x06: "accel. override", 0x07: "not available"}
+cruiseControl["cruiseControlState"] = pd.Series((cruiseControl["6"] & 0xE0) // 32).map(cruiseControlStatus)
+cruiseControlPtoStatus = {0x00: "off", 0x05: "set", 0x1F: "not available"}
+cruiseControl["cruiseControlPto"] = pd.Series(cruiseControl["6"] & 0x1F).map(cruiseControlPtoStatus)
+
+
+engineController1 = df[df["pgn"] == 0xF004]
+engineController1["engineTorque"] = engineController1["2"] - 125.0
+engineController1["engineSpeed"] = (engineController1["3"] + engineController1["4"] * 256) / 8.0
+
+engineController2 = df[df["pgn"] == 0xF003]
+engineController2["acceleratorPedal"] = engineController2["1"] * 0.4
+engineController2["engineLoad"] = engineController2["2"] * 0.4
+
+
+fig = make_subplots(rows=4, cols=1, shared_xaxes=True,
+                    vertical_spacing = 0.065,
+                    subplot_titles=["Vehicle Speed [km/h]",
+                                    "Accelerator Pedal [%]",
+                                    "Fuel Consumption [l]",
+                                    "  Door Open State        "])
 
 fig.add_trace(go.Scatter(x=tachograph["date"], y=tachograph["speed"],
                          name="Vehicle Speed [km/h]",
                          fill="tozeroy"), row=1, col=1)
 
+fig.add_trace(go.Scatter(x=engineController2["date"], y=engineController2["acceleratorPedal"],
+                          name="Accelerator Pedal [%]",
+                          fill="tozeroy"), row=2, col=1)
+
 fig.add_trace(go.Scatter(x=fuelConsumption["date"], y=fuelConsumption["fuelConsumption"],
                          name="Fuel Consumption [l]",
-                         fill="tozeroy"), row=2, col=1)
-
-fig.add_trace(go.Scatter(x=fuelEconomy["date"], y=fuelEconomy["fuelRate"],
-                         name="Fuel Rate [l/h]",
                          fill="tozeroy"), row=3, col=1)
 
-fig.add_trace(go.Scatter(x=fuelEconomy["date"], y=fuelEconomy["fuelEconomy"],
-                         name="Fuel Economy [km/l]",
-                         fill="tozeroy"), row=3, col=1)
 
-fig.add_trace(go.Scatter(x=doors["date"], y=doors["door1"],
-                          name="Door 1 Open State",
-                          fill="tozeroy"), row=4, col=1)
+doorStatus1 = {"closed": "1 closed", "open": "1 open"}
+doorStatus2 = {"closed": "2 closed", "open": "2 open"}
+doors["door1"] = pd.Series(doors["door1"]).map(doorStatus1)
+doors["door2"] = pd.Series(doors["door2"]).map(doorStatus2)
 
 fig.add_trace(go.Scatter(x=doors["date"], y=doors["door2"],
-                          name="Door 2 Open State",
-                          fill="tozeroy"), row=4, col=1)
+                          name="Door 2 Open State"), row=4, col=1)
 
-fig.add_trace(go.Scatter(x=suspension["date"], y=suspension["suspension"],
-                          name="Air Suspension Control [kPa]",
-                          fill="tozeroy"), row=5, col=1)
+fig.add_trace(go.Scatter(x=doors["date"], y=doors["door1"],
+                          name="Door 1 Open State"), row=4, col=1)
 
-fig.update_layout(width=1800, title_text=fileName)
+
+# fig.add_trace(go.Scatter(x=alternator["date"], y=alternator["alternatorSpeed"],
+#                           name="Alternator Speed [rpm]",
+#                           fill="tozeroy"), row=2, col=1)
+
+# fig.add_trace(go.Scatter(x=engineController1["date"], y=engineController1["engineSpeed"],
+#                           name="Engine Speed [rpm]",
+#                           fill="tozeroy"), row=2, col=1)
+
+
+# fig.add_trace(go.Scatter(x=fuelEconomy["date"], y=fuelEconomy["fuelRate"],
+#                          name="Fuel Rate [l/h]",
+#                          fill="tozeroy"), row=4, col=1)
+
+# fig.add_trace(go.Scatter(x=fuelEconomy["date"], y=fuelEconomy["fuelEconomy"],
+#                          name="Fuel Economy [km/l]",
+#                          fill="tozeroy"), row=5, col=1)
+
+# fig.add_trace(go.Scatter(x=engineController2["date"], y=engineController2["engineLoad"],
+#                           name="Engine Load [%]",
+#                           fill="tozeroy"), row=5, col=1)
+
+# fig.add_trace(go.Scatter(x=engineController1["date"], y=engineController1["engineTorque"],
+#                           name="Engine Torque [%]",
+#                           fill="tozeroy"), row=5, col=1)
+
+# fig.add_trace(go.Scatter(x=cruiseControl["date"], y=cruiseControl["breakSwitch"],
+#                           name="Break Pedal",
+#                           fill="tozeroy"), row=7, col=1)
+
+# fig.add_trace(go.Scatter(x=cruiseControl["date"], y=cruiseControl["clutchSwitch"],
+#                           name="Clutch Switch",
+#                           fill="tozeroy"), row=8, col=1)
+
+# fig.add_trace(go.Scatter(x=airPressure["date"], y=airPressure["airPressure"],
+#                           name="Air Supply Pressure [kPa]",
+#                           fill="tozeroy"), row=7, col=1)
+
+# fig.add_trace(go.Scatter(x=vehicleDistance["date"], y=vehicleDistance["vehicleDistance"],
+#                           name="Vehicle Distance [m]",
+#                           fill="tozeroy"), row=7, col=1)
+
+# fig.add_trace(go.Scatter(x=suspension["date"], y=suspension["suspension"],
+#                           name="Air Suspension Control [kPa]",
+#                           fill="tozeroy"), row=5, col=1)
+
+# fig.add_trace(go.Scatter(x=temperature["date"], y=temperature["temperature"],
+#                           name="Engine Temperature [°C]",
+#                           fill="tozeroy"), row=6, col=1)
+
+# fig.add_trace(go.Scatter(x=ambientAir["date"], y=ambientAir["ambientAir"],
+#                           name="Ambient Air [°C]",
+#                           fill="tozeroy"), row=7, col=1)
+
+# fig.add_trace(go.Scatter(x=diselExhaustFluid["date"], y=diselExhaustFluid["diselExhaustFluid"],
+#                           name="Diesel Exhaust Fluid Tank 1 Information [%]",
+#                           fill="tozeroy"), row=8, col=1)
+
+
+
+fig.update_annotations(font=dict(size=18))
+for i in fig.layout.annotations:
+    i.update(x=0.06, yshift=6)
+
+gray = "#CCCCCC"
+fig.update_layout(showlegend=False)
+fig.update_yaxes(ticks="outside", tickwidth=2, tickcolor='white', ticklen=5,
+                 linewidth=1.1, linecolor=gray, gridwidth=1.1, gridcolor=gray,
+                 tickfont=dict(size=15))
+fig.update_xaxes(ticks="outside", tickwidth=2, tickcolor='white', ticklen=10,
+                 linewidth=1.1, linecolor=gray, gridwidth=1.1, gridcolor=gray,
+                 tickfont=dict(size=15))
+
+fig['layout']['yaxis3'].tickvals = [0, 0.25, 0.5, 0.75, 1.0, 1.25]
+
+fig.update_layout(width=2000, height=1100, template="plotly_white")#, title_text=fileName)
 fig.show()
 fig.write_html("plot.html")
+fig.write_image("plot.svg")
---
+++ CAN-Dump Converter/dash_test.py
@@ -0,0 +1,55 @@
+import dash
+from dash.dependencies import Output, Input
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly
+import random
+import plotly.graph_objs as go
+from collections import deque
+import webbrowser
+import threading
+import time
+
+X = deque(maxlen=20)
+X.append(1)
+Y = deque(maxlen=20)
+Y.append(1)
+
+
+app = dash.Dash(__name__)
+app.layout = html.Div(
+    [
+        dcc.Graph(id='live-graph', animate=True),
+        dcc.Interval(
+            id='graph-update',
+            interval=1*1000
+        ),
+    ]
+)
+
+@app.callback(Output('live-graph', 'figure'),
+              [Input('graph-update', 'n_intervals')])
+def update_graph_scatter(input_data):
+    X.append(X[-1]+1)
+    Y.append(Y[-1]+Y[-1]*random.uniform(-0.1,0.1))
+
+    data = plotly.graph_objs.Scatter(
+            x=list(X),
+            y=list(Y),
+            name='Scatter',
+            mode= 'lines+markers'
+            )
+
+    return {'data': [data],'layout' : go.Layout(xaxis=dict(range=[min(X),max(X)]),
+                                                yaxis=dict(range=[min(Y),max(Y)]),)}
+
+
+
+if __name__ == '__main__':
+    port = 8080
+    threading.Timer(1, webbrowser.open_new("http://localhost:{}".format(port))).start();
+    #app.run_server(port=port, threaded=True, processes=1)
+    app.run_server(port=port)
+    #x = threading.Thread(target=app.run_server, kwargs={'port':port, 'threaded':True, 'processes':1})#, daemon=True)
+    input("wait on input...")
+    #x.terminate()
