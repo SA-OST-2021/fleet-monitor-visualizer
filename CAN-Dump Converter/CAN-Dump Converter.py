55da6687e1510102e845c2c8651c0ac3f0d9fdf4
Florian Baumgartner <florian.b-h@bluewin.ch>
2021-12-21 16:05:09 +0100

    Updated License

--- CAN-Dump Converter/CAN-Dump Converter.py
+++ CAN-Dump Converter/CAN-Dump Converter.py
@@ -9,7 +9,7 @@
 ###############################################################################
 # MIT License
 #
-# Copyright (c) 2021 Florian Baumgartner
+# Copyright (c) 2021 Institute for Networked Solutions OST
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
